5a2e8d557a62495388daebbe8444f9dfd3657048
leejoker <1056650571@qq.com>
2025-07-21 18:33:39 +0800

    fix bug

--- src/main/clojure/util/git.clj
+++ src/main/clojure/util/git.clj
@@ -10,6 +10,7 @@
 
 (defn clone-repo
   [url dir retry-times]
+  (System/setProperty "native.encoding" "UTF-8")
   (let [git           (Git/cloneRepository)
         non-empty-dir (and (fs/exists? dir) (> (count (filter #(not (fs/hidden? %)) (fs/list-dir dir))) 0))]
     (if non-empty-dir
@@ -21,11 +22,12 @@
         (try (with-open [g (-> git
                                (.setURI url)
                                (.setDirectory (fs/file dir))
+                               (.setCloneAllBranches false)
                                (.setProgressMonitor (TextProgressMonitor.))
                                (.call))]
                g)
              (catch Exception e
                (println "Retry " retry-times "times")
                (if (> retry-times 3)
-                 (println "Clone " url " to " (str dir) " failed: " (.getMessage e))
+                 (println "Clone " (s/replace url #"http://leejoker.top:11566/" "") " to " (str dir) " failed: " e)
                  (clone-repo url dir (inc retry-times)))))))))
